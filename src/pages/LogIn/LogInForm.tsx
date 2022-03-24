cbde90d6275b7d8b9f8a0a550344abd205eb4d71
Brayan Vargas <Brayan.Vargas@endava.com>
2022-03-24 14:39:19 -0500

    a login interface was added

--- src/pages/LogIn/LogInForm.tsx
+++ src/pages/LogIn/LogInForm.tsx
@@ -1,12 +1,33 @@
 import {Input} from "../../components/index"
+import {useForm, SubmitHandler} from "react-hook-form";
+import FieldLoginObject from "./login.interface";
+import {data} from "autoprefixer";
 
 const LogInForm = ()=>{
+
+    const {
+        register,
+        handleSubmit,
+        reset,
+        getValues,
+        formState: {errors}
+    } = useForm<FieldLoginObject>({mode: "onTouched"});
+
+    const onSubmit: SubmitHandler<FieldLoginObject> = (data) => {
+        console.log(data);
+        reset();
+    }
+
+
+
     return(
         <div className="flex justify-center w-1/2 mt-10">
             <div className="p-4 container-form  item-center  bg-white rounded-lg border shadow-md sm:p-8">
                 <form>
-                    <Input type="text" id="emailLogin" label="Email" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$"/>
-                    <Input type="password" id="password_login" label="Password"/>
+                    <Input type="text" id="email" label="Email"
+                           register={register}
+                           pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$"/>
+                    <Input type="password" id="password" label="Password"/>
                     <button
                         type="submit"
                         className="text-white color-endabank focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm  sm:w-full px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
---
+++ src/pages/LogIn/login.interface.ts
@@ -0,0 +1,8 @@
+interface LoginProps {
+    id?: string;
+    email: string;
+    password: string;
+
+}
+
+export default LoginProps;
