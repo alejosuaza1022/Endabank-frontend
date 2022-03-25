a086ecb3aba01d51e2ce9dd9825deec40d068de1
bmgabi95 <98468662+bmgabi95@users.noreply.github.com>
2022-03-25 17:50:26 -0500

    [MEDDV002-1848] Login and registerForm

---
+++ src/pages/LogIn/LogInForm.tsx
@@ -0,0 +1,70 @@
+import { Input, PopUp } from "../../components/index";
+import { useForm, SubmitHandler } from "react-hook-form";
+import LoginObject from "./loginObject.interface";
+import { Link } from "react-router-dom";
+
+const LogInForm = () => {
+  const {
+    register,
+    handleSubmit,
+    reset,
+    getValues,
+    formState: { errors },
+  } = useForm<LoginObject>({ mode: "onTouched" });
+
+  const onSubmit: SubmitHandler<LoginObject> = (data) => {
+    console.log(data);
+    reset();
+  };
+
+  return (
+    <div className="w-full">
+      <form>
+        <Input
+          type="text"
+          id="email"
+          label="Email"
+          register={register}
+          error={errors.email}
+          optionsValidations={{
+            pattern: {
+              value:
+                /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,})+$/,
+              message: "Email no valido",
+            },
+          }}
+        />
+        <Input
+          type="password"
+          id="password"
+          label="Password"
+          error={errors.password}
+          register={register}
+          optionsValidations={{
+            pattern: {
+              value: /^((?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[\W]).{8,20})/,
+              message:
+                "1 Mayuscula, 1 Caracter Especial, 1 Número, 8 a 20 digitos",
+            },
+          }}
+        />
+        <button
+          type="submit"
+          className="text-white color-endabank focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm  sm:w-full px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+        >
+          Log In
+        </button>
+        <div className="m-5 text-right text-sm">
+          <a className="decoration-0" href="www.nowhere.com" data-modal-toggle="authentication-modal">
+            Forgot password?
+          </a>
+        </div>
+      </form>
+      <div className="text-center text-sm m-0 border-t-2 border-gray-300 pt-5">
+        New merchant? <Link to="/signup">create an account</Link>
+      </div>
+    </div>
+  );
+};
+
+export default LogInForm;
--- src/pages/RegisterForm/Form.tsx
+++ src/pages/RegisterForm/Form.tsx
@@ -1,6 +1,6 @@
 import "./index.css";
 import { Input, MainImage, SelectForm } from "../../components/index";
-import FieldObject from "./userObject.interface";
+import UserObject from "./userObject.interface";
 import { useForm, SubmitHandler } from "react-hook-form";
 const Form = () => {
   const {
@@ -9,152 +9,116 @@ const Form = () => {
     reset,
     getValues,
     formState: { errors },
-  } = useForm<FieldObject>({ mode: "onTouched" });
+  } = useForm<UserObject>({ mode: "onTouched" });
 
-  const onSubmit: SubmitHandler<FieldObject> = (data) => {
+  const onSubmit: SubmitHandler<UserObject> = (data) => {
     console.log(data);
     reset();
   };
 
   return (
     <>
-      <MainImage></MainImage>
-      <div className="flex w-full justify-center mt-10">
-        <div className="p-4  container-form  item-center  bg-white rounded-lg border shadow-md sm:p-8">
-          <form onSubmit={handleSubmit(onSubmit)}>
-            <div className="grid-cc">
-              <SelectForm
-                id="documentType"
-                value="Document type"
-                options={["CC", "CE"]}
-                error={errors.documentType}
-                {...register("documentType", {
-                  required: {
-                    value: true,
-                    message: "Este campo no puede estar vacio",
-                  },
-                })}
-              ></SelectForm>
-              <Input
-                type="text"
-                id="id"
-                label="Identifier"
-                {...register("id", {
-                  required: {
-                    value: true,
-                    message: "Este campo no puede estar vacio",
-                  },
-                  pattern: {
-                    value: /^[0-9]{10,20}/,
-                    message:
-                      "El campo acepta solo números y deben ser de 10 a 20 caracteres",
-                  },
-                })}
-                error={errors.id}
-              ></Input>
-            </div>{" "}
-            <div className="grid xl:grid-cols-2 xl:gap-6">
-              <Input
-                type="text"
-                id="firstName"
-                label="First name"
-                error={errors.firstName}
-                {...register("firstName", {
-                  required: {
-                    value: true,
-                    message: "Este campo no puede estar vacio",
-                  },
-                })}
-              ></Input>
-              <Input
-                type="text"
-                id="lastName"
-                label="Last name"
-                error={errors.lastName}
-                {...register("lastName", {
-                  required: {
-                    value: true,
-                    message: "Este campo no puede estar vacio",
-                  },
-                })}
-              ></Input>
-            </div>{" "}
-            <Input
-              type="tel"
-              id="phoneNumber"
-              label="Phone Number"
-              error={errors.phoneNumber}
-              {...register("phoneNumber", {
-                required: {
-                  value: true,
-                  message: "Este campo no puede estar vacio",
-                },
-                pattern: {
-                  value: /^[0-9]{10}/,
-                  message:
-                    "El campo acepta solo números y deben ser de 10 caracteres",
-                },
-              })}
-            ></Input>
-            <Input
-              type="text"
-              id="email"
-              label="Email"
-              error={errors.email}
-              {...register("phoneNumber", {
-                required: {
-                  value: true,
-                  message: "Este campo no puede estar vacio",
-                },
-                pattern: {
-                  value:
-                    /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,})+$/,
-                  message: "Email no valido",
-                },
-              })}
-            ></Input>
-            <div className="grid xl:grid-cols-2 xl:gap-6">
-              <Input
-                type="password"
-                id="password"
-                label="Password"
-                error={errors.password}
-                {...register("phoneNumber", {
-                  required: {
-                    value: true,
-                    message: "Este campo no puede estar vacio",
-                  },
-                  pattern: {
-                    value: /^((?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[\W]).{8,20})/,
-                    message:
-                      "1 Mayuscula, 1 Caracter Especial, 1 Número, 8 a 20 digitos",
-                  },
-                })}
-              ></Input>
-              <Input
-                type="password"
-                id="rePassword"
-                label="Confirm password"
-                error={errors.rePassword}
-                {...register("phoneNumber", {
-                  required: {
-                    value: true,
-                    message: "Este campo no puede estar vacio",
-                  },
-                  validate: () =>
-                    getValues("password") === getValues("rePassword"),
-                })}
-              ></Input>
-            </div>
-            <button
-              type="submit"
-              className="text-white color-endabank  focus:ring-4  font-medium rounded-lg text-sm  w-full px-5 py-2.5 text-center "
-            >
-              Submit
-            </button>
-          </form>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div className="grid-cc">
+          <SelectForm
+            id="documentType"
+            value="Document type"
+            options={["CC", "CE"]}
+            error={errors.documentType}
+            register={register}
+          ></SelectForm>
+          <Input
+            type="text"
+            id="id"
+            label="Identifier"
+            register={register}
+            error={errors.id}
+            optionsValidations={{
+              pattern: {
+                value: /^[0-9]{10,20}/,
+                message:
+                  "El campo acepta solo números y deben ser de 10 a 20 caracteres",
+              },
+            }}
+          ></Input>
+        </div>{" "}
+        <div className="grid xl:grid-cols-2 xl:gap-6">
+          <Input
+            type="text"
+            id="firstName"
+            label="First name"
+            error={errors.firstName}
+            register={register}
+          ></Input>
+          <Input
+            type="text"
+            id="lastName"
+            label="Last name"
+            error={errors.lastName}
+            register={register}
+          ></Input>
+        </div>{" "}
+        <Input
+          type="tel"
+          id="phoneNumber"
+          label="Phone Number"
+          error={errors.phoneNumber}
+          register={register}
+          optionsValidations={{
+            pattern: {
+              value: /^[0-9]{10,20}/,
+              message:
+                "El campo acepta solo números y deben ser de 10 a 20 caracteres",
+            },
+          }}
+        ></Input>
+        <Input
+          type="text"
+          id="email"
+          label="Email"
+          error={errors.email}
+          register={register}
+          optionsValidations={{
+            pattern: {
+              value:
+                /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,})+$/,
+              message: "Email no valido",
+            },
+          }}
+        ></Input>
+        <div className="grid xl:grid-cols-2 xl:gap-6">
+          <Input
+            type="password"
+            id="password"
+            label="Password"
+            error={errors.password}
+            register={register}
+            optionsValidations={{
+              pattern: {
+                value: /^((?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[\W]).{8,20})/,
+                message:
+                  "1 Mayuscula, 1 Caracter Especial, 1 Número, 8 a 20 digitos",
+              },
+            }}
+          ></Input>
+          <Input
+            type="password"
+            id="rePassword"
+            label="Confirm password"
+            error={errors.rePassword}
+            register={register}
+            optionsValidations={{
+              validate: () => getValues("password") === getValues("rePassword"),
+            }}
+          ></Input>
         </div>
-      </div>
+        <button
+          type="submit"
+          className="text-white color-endabank  focus:ring-4  font-medium rounded-lg text-sm  w-full px-5 py-2.5 text-center "
+        >
+          Submit
+        </button>
+      </form>
     </>
   );
 };
