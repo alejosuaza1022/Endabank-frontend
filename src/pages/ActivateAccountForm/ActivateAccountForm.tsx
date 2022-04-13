50569fc0ed6ef9c44d43a73b3627108ff117d69f
AnthonyDGallego <Anthony.Gallego@endava.com>
2022-04-13 14:01:47 -0500

    Update ActivateAccountForm.tsx

--- src/pages/ActivateAccountForm/ActivateAccountForm.tsx
+++ src/pages/ActivateAccountForm/ActivateAccountForm.tsx
@@ -3,19 +3,17 @@ import apiUrls from "../../constants/apiUrls";
 import { useEffect, useState } from "react";
 import { getAxios } from "../../utils/axios";
 import { ApproveUserTable } from "../../components/index";
-
 const ActivateAccountForm = () => {
   const [list, setList] = useState<Array<ApproveUserProps>>([]);
   const [appr, setAppr] = useState(Boolean);
   const token =
-    "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJhbnRob255LmdhbGxlZ29AZW5kYXZhLmNvbSIsImV4cCI6MTY0OTI2MDUyNSwiaWF0IjoxNjQ5MjU5MzI1LCJ1c2VySWQiOjV9.QDIrF4v8s_ffVhSkqktF3Cus2g0uSitpbqVQ_mGNlPQu6hH-8kLare39hhnUoOeLcXsn1E4ZrmgoEH_Ax0Uc7g";
+    "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJhbnRob255LmdhbGxlZ29AZW5kYXZhLmNvbSIsImV4cCI6MTY0OTg2Mzg4OSwiaWF0IjoxNjQ5ODYyNjg5LCJ1c2VySWQiOjV9.F-08ShjxIBT8Qh8IDGsU-OtNy9Q9RnfIg5EnFNEzhp-jbsPIUjD58HEQNQ0OifTUGDDEWCtCw-LK_kf3Kk9y5A";
   useEffect(() => {
     async function getData() {
       setAppr(true);
       try {
         const response: Array<ApproveUserProps> = await getAxios(
           apiUrls.GET_USERS_TO_APPROVE_URL,
-
           token
         );
 
@@ -76,70 +74,7 @@ const ActivateAccountForm = () => {
           </tbody>
         </table>
       </div>
-
-      <nav aria-label="Page navigation example">
-        <ul className="inline-flex -space-x-px w-full justify-center mt-10">
-          <li>
-            <a
-              href="#"
-              className="py-2 px-3 ml-0 leading-tight text-gray-500 bg-white rounded-l-lg border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
-            >
-              Previous
-            </a>
-          </li>
-          <li>
-            <a
-              href="#"
-              className="py-2 px-3 text-blue-600 bg-blue-50 border border-gray-300 hover:bg-blue-100 hover:text-blue-700 dark:border-gray-700 dark:bg-gray-700 dark:text-white"
-            >
-              1
-            </a>
-          </li>
-          <li>
-            <a
-              href="#"
-              className="py-2 px-3 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
-            >
-              2
-            </a>
-          </li>
-          <li>
-            <a
-              href="#"
-              aria-current="page"
-              className="py-2 px-3 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
-            >
-              3
-            </a>
-          </li>
-          <li>
-            <a
-              href="#"
-              className="py-2 px-3 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
-            >
-              4
-            </a>
-          </li>
-          <li>
-            <a
-              href="#"
-              className="py-2 px-3 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
-            >
-              5
-            </a>
-          </li>
-          <li>
-            <a
-              href="#"
-              className="py-2 px-3 leading-tight text-gray-500 bg-white rounded-r-lg border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
-            >
-              Next
-            </a>
-          </li>
-        </ul>
-      </nav>
     </div>
   );
 };
 
-export default ActivateAccountForm;
