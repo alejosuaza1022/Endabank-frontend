2e15364f49929d6369f97ba4daff0c46ddcd49e0
Alejandro Suaza Builes <Alejandro.SuazaBuiles@endava.com~>
2022-04-21 11:58:04 -0500

    bugFix, colors not the same on errors and password errors solved

--- src/components/InputForm/InputForm.tsx
+++ src/components/InputForm/InputForm.tsx
@@ -73,15 +73,15 @@ const InputForm: React.FC<InputProps> = ({
             </label>
 
             {error && error.type === "validate" && id === "newPassword" ? (
-                <p className="text-xs text-red-600 ml-1 mt-2 text-bold">
+                <p className="text-xs text-red-500 ml-1 mt-2 text-bold">
                     The old password and new password must be different
                 </p>
             ) : error && error.type === "validate" && id === "rePassword" ? (
-                <p className="text-xs text-red-600 ml-1 mt-2 text-bold">
+                <p className="text-xs text-red-500 ml-1 mt-2 text-bold">
                     Passwords do not match
                 </p>
             ) : error ? (
-                <p className="text-xs text-red-600 ml-1 mt-2 text-bold">{error.message}</p>
+                <p className="text-xs text-red-500 ml-1 mt-2 text-bold">{error.message}</p>
             ) : null}
         </div>
     );
