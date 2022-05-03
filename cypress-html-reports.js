76f7db25e61cc2b1229dfaab29c1a6a68c57c2e5
angelicamarmolejo28 <50608602+angelicamarmolejo28@users.noreply.github.com>
2022-05-03 17:58:40 -0500

    resolving conflicting changes

--- cypress-html-reports.js
+++ cypress-html-reports.js
@@ -2,11 +2,8 @@ const report = require("multiple-cucumber-html-reporter");
 
 report.generate({
   jsonDir: "cypress/cucumber-json",
-<<<<<<< HEAD
-  reportPath: "cypress/reports/cucumber-htmlreport.html",
-=======
+  
   reportPath: "./reports/cucumber-htmlreport.html",
->>>>>>> origin/development
   metadata: {
     browser: {
       name: "chrome",
@@ -18,8 +15,5 @@ report.generate({
       version: "10",
     },
   },
-<<<<<<< HEAD
+
 });
-=======
-});
->>>>>>> origin/development
