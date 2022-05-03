624d6d24a79c2a14cfe996ced88621b16bd1d0a4
J. Var <bramf0@protonmail.com>
2022-05-03 13:58:40 -0500

    modify sidebar height and the order of sidebar logout element

--- src/components/SideBar/SideBar.tsx
+++ src/components/SideBar/SideBar.tsx
@@ -6,7 +6,7 @@ const SideBar = () => {
   const {logOut,auth} = useAuth();
 
   return(
-      <aside className="w-64" aria-label="Sidebar">
+      <aside className="flex w-64" aria-label="Sidebar">
         <div className="overflow-y-auto py-4 px-3 bg-gray-50 rounded dark:bg-gray-800">
           <ul className="space-y-2">
             <li>
@@ -31,17 +31,6 @@ const SideBar = () => {
                 <span className="flex-1 ml-3 whitespace-nowrap">Password management</span>
               </Link>
             </li>
-            <li>
-              <Link id="logoutSidebarElement" to="/" onClick={logOut}
-                 className="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700">
-                <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24"
-                     xmlns="http://www.w3.org/2000/svg">
-                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
-    d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1"/>
-                </svg>
-                <span className="flex-1 ml-3 whitespace-nowrap">Log out</span>
-              </Link>
-            </li>
             {auth?.authorities?.find(role => ['ROLE_ADMIN'].includes(role)) &&
                 <li>
                   <Link id="usersManagementSidebarElement" to="/activate-account"
@@ -54,6 +43,17 @@ const SideBar = () => {
                     <span className="flex-1 ml-3 whitespace-nowrap">Users management</span>
                   </Link>
                 </li>}
+            <li>
+              <Link id="logoutSidebarElement" to="/" onClick={logOut}
+                    className="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700">
+                <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24"
+                     xmlns="http://www.w3.org/2000/svg">
+                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
+                        d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1"/>
+                </svg>
+                <span className="flex-1 ml-3 whitespace-nowrap">Log out</span>
+              </Link>
+            </li>
           </ul>
         </div>
       </aside>
