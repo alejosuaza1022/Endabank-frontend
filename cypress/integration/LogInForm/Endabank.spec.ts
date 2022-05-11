8f98cd032fa15017db4dbb28dcd010d1e8db8420
Angelica Marmolejo <Angelica.Marmolejo@endava.com>
2022-05-11 16:04:07 -0500

    Creating a basic setup for testing with Cucumber and Cypress

--- cypress/integration/LogInForm/Endabank.spec.ts
+++
@@ -1,7 +0,0 @@
-import { Given } from "cypress-cucumber-preprocessor/steps";
-
-const url = 'http://localhost:3000/log-in/'
-Given('I open Endabank login page and wants to sign in', () => {
-    cy.visit(url)
-    cy.wait(1000)
-})
