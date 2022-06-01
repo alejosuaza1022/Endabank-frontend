d8340677a32c5cddcc15e67fe3fc3667432de137
J. Var <bramf0@protonmail.com>
2022-06-01 12:56:32 -0500

    create section title component

---
+++ src/components/SectionTitle/SectionTitle.tsx
@@ -0,0 +1,11 @@
+
+const SectionTitle = ({title}: {title: string}) => {
+  return(
+      <div className="border-b border-gray-300 mb-5" >
+          <h5 className="mb-2 text-2xl font-bold">{title}</h5>
+      </div>
+
+  );
+}
+
+export default SectionTitle;
