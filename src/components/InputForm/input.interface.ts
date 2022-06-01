5002ead3cbc832fba6c92317a2ee8d7ee7b025bb
J. Var <bramf0@protonmail.com>
2022-06-01 13:02:39 -0500

    add merchant form interface to input component interface

--- src/components/InputForm/input.interface.ts
+++ src/components/InputForm/input.interface.ts
@@ -2,10 +2,11 @@ import UserObject from "@pages/RegisterForm/userObject.interface";
 import { FieldError, UseFormRegister, ValidationRule } from "react-hook-form";
 import LoginObject from "../../pages/LogIn/loginObject.interface";
 import resetPasswordObject from "../../pages/FormResetPassword/resetPasswordObject.interface";
+import MerchantFormObject from "../../pages/MerchantInfo/merchantFormObject.interface";
 
 interface InputProps {
   type: string;
-  id: keyof UserObject | keyof LoginObject | keyof resetPasswordObject;
+  id: keyof UserObject | keyof LoginObject | keyof resetPasswordObject | keyof MerchantFormObject;
   label: string;
   error?: FieldError;
   register: UseFormRegister<any>;
