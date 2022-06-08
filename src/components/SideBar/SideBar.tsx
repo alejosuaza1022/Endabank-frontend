14bbaf7963ef2383c822e26b29b800fb4db07765
J. Var <bramf0@protonmail.com>
2022-06-07 23:16:14 -0500

    implement rendering conditional for 'MerchantKeysSection' component

--- src/components/SideBar/SideBar.tsx
+++ src/components/SideBar/SideBar.tsx
@@ -48,7 +48,7 @@ const SideBar = () => {
                     className="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700">
                 <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24"
                      xmlns="http://www.w3.org/2000/svg">
-                  <path strokeLinecap="round" strokeLinejoin="round" stroke-width="2"
+                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
                         d="M16 11V7a4 4 0 00-8 0v4M5 9h14l1 12H4L5 9z"/>
                 </svg>
                 <span className="flex-1 ml-3 whitespace-nowrap">Became a merchant</span>
