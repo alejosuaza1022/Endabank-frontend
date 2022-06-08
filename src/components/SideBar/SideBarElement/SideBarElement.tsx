e7db0949589a8f5f7a5d78c0c30295b015d25963
J. Var <bramf0@protonmail.com>
2022-06-08 08:56:42 -0500

    add id attribute to 'sidebarElement' component

--- src/components/SideBar/SideBarElement/SideBarElement.tsx
+++ src/components/SideBar/SideBarElement/SideBarElement.tsx
@@ -2,11 +2,11 @@ import {Link} from "react-router-dom";
 import React from "react";
 import SideBarProps from "@components/SideBar/SideBarElement/sideBarElement.interface";
 
-const SideBarElement:React.FC<SideBarProps> = ({
+const SideBarElement:React.FC<SideBarProps> = ({id,
     name,icon,route,onClick,
                         }) => {
     return(
-        <Link id="profileSidebarElement" to={route} onClick={onClick}
+        <Link id={id} to={route} onClick={onClick}
               className="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700">
             <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24"
                  xmlns="http://www.w3.org/2000/svg">
