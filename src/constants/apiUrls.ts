dcb601ef19c6e07e3a92588929093f2fd1caac9b
J. Var <bramf0@protonmail.com>
2022-06-07 23:13:29 -0500

    add in apiUrls constants the route for general user info endpoint

--- src/constants/apiUrls.ts
+++ src/constants/apiUrls.ts
@@ -2,6 +2,7 @@ class apiUrls {
   static readonly BASE_URL = "http://localhost:8080/api/v1";
   static readonly GET_USERS_TO_APPROVE_URL = `${this.BASE_URL}/users`;
   static readonly GET_USER_DETAILS = `${this.BASE_URL}/users/details`;
+  static readonly GET_USER_GENERAL_INFO = `${this.BASE_URL}/users/general-info`;
   static readonly PUT_USERS_TO_APPROVE_URL = `${this.GET_USERS_TO_APPROVE_URL}/approve`;
   static readonly LOG_IN_URL = `${this.BASE_URL}/login`
   static readonly GET_USERS_CHANGE_PASSWORD_URL = `${this.BASE_URL}/users/change-password`;
