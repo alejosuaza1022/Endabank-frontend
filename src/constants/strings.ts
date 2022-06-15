338725d035eee04ec51b3bce80a8ca11b38c0eca
J. Var <bramf0@protonmail.com>
2022-06-15 14:17:05 -0500

    modify success message in 'register merchant' request and add validation to tax id field

--- src/constants/strings.ts
+++ src/constants/strings.ts
@@ -3,6 +3,7 @@ class Strings {
     static readonly COLOR_SUCCESS = "green";
     static readonly COLOR_WARNING = "yellow";
     static readonly USER_REGISTERED = "The user was registered successfully.";
+    static readonly MERCHANT_REQUEST_REGISTERED = "The request to become a merchant was registered successfully.";
     static readonly ERROR_MESSAGE = "There was an issue with the request. Please try again later.";
     static readonly TOGGLE_MESSAGE = "toggle"
     static readonly LETS_VERIFY_EMAIL = "Let's verify your email address.";
