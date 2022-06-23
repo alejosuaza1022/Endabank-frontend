cb4176194cbdedac1d153b5a45cac60aa7a32f1e
Jaime Gonzalez <Jaime.Gonzalez@endava.com>
2022-06-23 18:28:59 -0500

    solving conflicts 3.0

--- cypress/plugins/index.ts
+++ cypress/plugins/index.ts
@@ -8,11 +8,7 @@ const path = require("path");
  * @type {Cypress.PluginConfig}
  */
 // eslint-disable-next-line no-unused-vars
-<<<<<<< HEAD
 module.exports = (on:any, config:any) => {
-=======
-module.exports = (on, config) => {
->>>>>>> 667afdfa965b0be97da0d46978f80808f5a2e9db
  const options = {
  ...browserify.defaultOptions,
  typescript: path.join(path.resolve("."), "node_modules/typescript"),
