11953f8f6e836f9e947575fa0ffa61d9b27f7081
J. Var <bramf0@protonmail.com>
2022-06-28 15:37:08 -0500

    implement request to get all merchant requests

--- src/pages/MerchantRequestsCollection/MerchantRequestsCollection.tsx
+++ src/pages/MerchantRequestsCollection/MerchantRequestsCollection.tsx
@@ -1,28 +1,43 @@
-import {PopUpMessage, Spinner} from "../../components/index";
-import {useState} from "react";
-import Strings from "../../constants/strings";
+import {Spinner} from "../../components/index";
+import {useEffect, useState} from "react";
 import Cookies from "js-cookie";
 import {Navigate} from "react-router-dom";
 import RequestsTable from "./RequestsTable";
+import ReactPaginate from "react-paginate";
+import MerchantRequestPaginationDataProps
+    from "./RequestsTable/merchantRequestPaginationDataProps.interface";
+import {getAxios} from "../../utils/axios";
+import apiUrls from "../../constants/apiUrls";
 
 const MerchantRequestsCollection = () => {
 
     const token = Cookies.get("token");
     const [isLoading, setIsLoading] = useState(false);
+    const [merchantRequests, setMerchantRequests] = useState<MerchantRequestPaginationDataProps>();
 
-    const [showPopUpMessage, setShowPopUpMessage] = useState(false);
-    const [messagePopUp, setMessagePopUp] = useState<string>(Strings.USER_REGISTERED);
-    const [isColorError, setIsColorError] = useState<boolean>(false);
 
-    const getMerchantRequests = (page: number) => {
+    const getMerchantRequests = async (page: number) => {
         try{
             setIsLoading(true);
 
-
+            const res: MerchantRequestPaginationDataProps = await getAxios(
+                apiUrls.GET_MERCHANT_REQUESTS+"/"+page,
+                token
+            );
+            setIsLoading(false);
+            setMerchantRequests(res);
 
         } catch (e) {
             setIsLoading(false);
         }
+    }
+
+    useEffect(() => {
+        getMerchantRequests(0)
+    }, []);
+
+    const handlePageClick = (data:any) => {
+        getMerchantRequests(data.selected);
     }
 
     const renderPageOrLoading = () => {
@@ -52,11 +67,43 @@ const MerchantRequestsCollection = () => {
                         </tr>
                         </thead>
                         <tbody>
-                            <RequestsTable id="1" merchantRequestStateName="approved"
-                                           createAt="1/2/3" storeName="shoes"  />
+                        { merchantRequests?.content.map((value,key)=>(
+
+                            <RequestsTable id={value.id}
+                                           merchantRequestStateName={value.merchantRequestStateName}
+                                           createAt={value.createAt}
+                                           storeName={value.storeName}
+                                           approveOptionState={value.merchantRequestStateName === "APPROVED"}
+                                           rejectOptionState={value.merchantRequestStateName === "REJECTED"}
+                                           token = {token}
+                                           key = {key}
+                            />
+                        ))
+
+                        }
+
                         </tbody>
                     </table>
                 </div>
+                <div className="flex justify-center">
+                    {}
+                    <ReactPaginate
+                        previousLabel={"<"}
+                        nextLabel={">"}
+                        breakLabel={"..."}
+                        pageCount={merchantRequests != null ? merchantRequests.totalPages : 0}
+                        marginPagesDisplayed={2}
+                        pageRangeDisplayed={2}
+                        onPageChange={handlePageClick}
+                        containerClassName={"inline-flex justify-content-center -space-x-px"}
+                        pageClassName={"py-2 px-3 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"}
+                        previousClassName={"py-2 px-3 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"}
+                        nextClassName={"py-2 px-3 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"}
+                        breakClassName={"py-2 px-3 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"}
+                        activeClassName={"py-2 px-3 text-blue-600 bg-blue-50 border border-gray-300 hover:bg-blue-100 hover:text-blue-700 dark:border-gray-700 dark:bg-gray-700 dark:text-white"}
+                    />
+                </div>
+
             </div>
         )
     }
@@ -64,12 +111,7 @@ const MerchantRequestsCollection = () => {
     return(
         <>
             {token?.length != 0 ? renderPageOrLoading() : <Navigate replace to="/" />}
-            {showPopUpMessage && (
-                <div className="fixed bottom-0 right-0 lg:w-1/4 md:w-1/3  ">
-                    <PopUpMessage message={messagePopUp} setShowPopUpMessage={setShowPopUpMessage}
-                                  isColorError={isColorError} />
-                </div>
-            )}
+
         </>
     );
 }
